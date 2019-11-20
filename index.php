be91daf26f1f88d985dea8e5a0dcb49363f5f381
liam <dedypips@gmail.com>
2019-11-20 10:22:51 -0600

    Added PHP-view to the project.

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . './vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$app->redirect('/index', '/');
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    $renderer = new PhpRenderer('./templates');
+    return $renderer->render($response,'index.html', $args);
 });
 
 $app->get('/api/generate_weapon', function (Request $request, Response $response, $args) {
